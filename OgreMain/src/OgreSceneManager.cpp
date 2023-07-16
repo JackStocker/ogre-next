5d52898f66342ab81f00695a1fc91404b50a49fa
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2023-07-16 18:59:29 -0300

    ParticleSystems are now added to RenderQueues and recognized by Hlms
    Create the VAO.
    Use an Index Buffer shared by all particle systems.
    Add MathlibSSE2::ToSnorm8Unsafe & extractS8.
    Still WIP.
    It will crash if you try to add a ParticleSystem.

--- OgreMain/src/OgreSceneManager.cpp
+++ OgreMain/src/OgreSceneManager.cpp
@@ -2119,6 +2119,8 @@ namespace Ogre
                 ObjectData objData;
                 const size_t totalObjs = memoryManager->getFirstObjectData( objData, i );
 
+                const uint8 currRqId = static_cast<uint8>( i );
+
                 // Skip if totalObjs == 0u. Profiling shows there is considerable gains.
                 // Too much (255 queues, most of them empty, multiples scene passes...)
                 if( totalObjs > 0u )
@@ -2138,8 +2140,6 @@ namespace Ogre
 
                     MovableObject::cullFrustum( numObjs, objData, camera, outVisibleObjects,
                                                 preparedData );
-
-                    const uint8 currRqId = static_cast<uint8>( i );
 
                     if( mRenderQueue->getRenderQueueMode( currRqId ) == RenderQueue::FAST &&
                         request.addToRenderQueue )
@@ -2169,6 +2169,13 @@ namespace Ogre
 
                         outVisibleObjects.clear();
                     }
+                }
+
+                if( mRenderQueue->getRenderQueueMode( currRqId ) == RenderQueue::PARTICLE_SYSTEM &&
+                    request.addToRenderQueue )
+                {
+                    mParticleSystemManager2->_addToRenderQueue( threadIdx, mNumWorkerThreads,
+                                                                mRenderQueue, currRqId, visibilityMask );
                 }
             }
 
