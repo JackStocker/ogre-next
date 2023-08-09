b232e2ff589544b3864f5d6c0db5f5a023dff30c
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2023-08-09 16:47:23 -0300

    Add a ParticleSystemManager2 where parsed scripts go

--- OgreMain/src/OgreScriptTranslator.cpp
+++ OgreMain/src/OgreScriptTranslator.cpp
@@ -6290,8 +6290,8 @@ namespace Ogre{
 
         if( !processed )
         {
-            ParticleSystemManager2 *particleSystemManager = 0;
-            mSystem = particleSystemManager->createParticleSystemDef( obj->name );
+            mSystem =
+                Root::getSingleton().getParticleSystemManager2()->createParticleSystemDef( obj->name );
         }
 
         if( !mSystem )
