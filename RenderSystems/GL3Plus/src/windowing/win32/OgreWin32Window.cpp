f6bffc3b806b6b7035ef3762dd2b504937f44704
Jack <jackstocker.93@gmail.com>
2023-09-14 20:13:17 +0100

    Champion
    Remove WS_EX_TOPMOST from fullscreen Win32 windows, doesn't work for non-desktop-resolution windows though

--- RenderSystems/GL3Plus/src/windowing/win32/OgreWin32Window.cpp
+++ RenderSystems/GL3Plus/src/windowing/win32/OgreWin32Window.cpp
@@ -346,7 +346,7 @@ namespace Ogre {
 
             if (mIsFullScreen)
             {               
-                dwStyleEx |= WS_EX_TOPMOST;
+                //dwStyleEx |= WS_EX_TOPMOST;
                 mTop = monitorInfoEx.rcMonitor.top;
                 mLeft = monitorInfoEx.rcMonitor.left;                                           
             }
